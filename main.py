3a8bbce48c9612ab1bfd8283ad2d79b9554af4b3
zadvornaya <yellowdragonjoy@gmail.com>
2020-10-02 20:02:56 +0400

    Зашифровка сообщения

--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import string
 import numpy as np
 
 
+def arrayToString(array):
+    return "".join(s for s in np.array_str(array) if s not in "[ ]'" and s.isprintable())
+
+
 def generateGrille(n: int):
     size = n * 2
     grille = np.zeros((size, size), dtype='uint8')
 
+    # Прямой обход (0)
     seq = np.arange(1, n * n + 1)
     random.shuffle(seq)
-    quant = random.randint(1, seq.shape[0] // 2)
-    # print(seq, " ", quant)
+    quant = random.randint(1, seq.shape[0] // 2) if seq.shape[0] > 1 else 0
     k = 1
     for i in range(size // 2):
         for j in range(size // 2):
@@ -19,9 +23,9 @@ def generateGrille(n: int):
                 grille[i, j] = 1
             k += 1
 
+    # Обход при повороте 90
     seq = seq[quant:]
-    quant = random.randint(1, seq.shape[0] // 2) if seq.shape[0] > 0 else 0
-    # print(seq, " ", quant)
+    quant = random.randint(1, seq.shape[0] // 2) if seq.shape[0] > 1 else 0
     k = 1
     for i in range(size // 2):
         for j in range(size // 2):
@@ -29,9 +33,9 @@ def generateGrille(n: int):
                 grille[j, size - i - 1] = 1
             k += 1
 
+    # Обход при повороте 180
     seq = seq[quant:]
-    quant = random.randint(1, seq.shape[0] // 2) if seq.shape[0] > 0 else 0
-    # print(seq, " ", quant)
+    quant = random.randint(1, seq.shape[0] // 2) if seq.shape[0] > 1 else 0
     k = 1
     for i in range(size // 2):
         for j in range(size // 2):
@@ -39,9 +43,9 @@ def generateGrille(n: int):
                 grille[size - i - 1, size - j - 1] = 1
             k += 1
 
+    # Обход при повороте 270
     seq = seq[quant:]
     quant = seq.shape[0]
-    # print(seq, " ", quant)
     k = 1
     for i in range(size // 2):
         for j in range(size // 2):
@@ -49,14 +53,15 @@ def generateGrille(n: int):
                 grille[size - j - 1, i] = 1
             k += 1
 
-    # print(grille)
     return grille
 
 
 def codeByCardanGrille(msg: str, n: int):
+    msg = msg.replace(' ', '_')
     grille = generateGrille(n)
     codedGrl = np.zeros(grille.shape, 'U1')
     size = n * 2
+    symbols = string.ascii_letters + string.digits + "_.,!?"
 
     # Прямой обход (0)
     for i in range(size):
@@ -66,10 +71,8 @@ def codeByCardanGrille(msg: str, n: int):
                     letter = msg[0]
                     msg = msg[1:]
                 else:
-                    letter = random.choice(string.ascii_letters)
+                    letter = random.choice(symbols)
                 codedGrl[i, j] = letter
-
-    print(codedGrl)
 
     # Обход при повороте 90
     for i in range(size):
@@ -79,9 +82,9 @@ def codeByCardanGrille(msg: str, n: int):
                     letter = msg[0]
                     msg = msg[1:]
                 else:
-                    letter = random.choice(string.ascii_letters)
-                codedGrl[j, size - i - 1] = letter
-    print(codedGrl)
+                    letter = random.choice(symbols)
+                codedGrl[i, j] = letter
+
     # Обход при повороте 180
     for i in range(size):
         for j in range(size):
@@ -90,9 +93,9 @@ def codeByCardanGrille(msg: str, n: int):
                     letter = msg[0]
                     msg = msg[1:]
                 else:
-                    letter = random.choice(string.ascii_letters)
-                codedGrl[size - i - 1, size - j - 1] = letter
-    print(codedGrl)
+                    letter = random.choice(symbols)
+                codedGrl[i, j] = letter
+
     # Обход при повороте 270
     for i in range(size):
         for j in range(size):
@@ -101,12 +104,20 @@ def codeByCardanGrille(msg: str, n: int):
                     letter = msg[0]
                     msg = msg[1:]
                 else:
-                    letter = random.choice(string.ascii_letters)
-                codedGrl[size - j - 1, i] = letter
+                    letter = random.choice(symbols)
+                codedGrl[i, j] = letter
 
     # return [str(codedGrl), str(grille)]
     return [codedGrl, grille]
 
 
 if __name__ == '__main__':
-    print(codeByCardanGrille("This string", 3))
+    print("Введите исходный текст:")
+    msg = input()
+    print("Введите размер малой решетки Кардано:")
+    n = int(input())
+
+    codedMsg = codeByCardanGrille(msg, n)
+
+    print(arrayToString(codedMsg[0]))
+    print(arrayToString(codedMsg[1]))
